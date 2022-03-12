eb3203d038c408fbc9f87154726529969afea44b
ChefDimas <dimalunik@aol.com>
2022-03-12 19:47:07 +0200

    Changes

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Product from "./product";
+import Product from "./Product";
 
 export default class ShoppingComponent extends Component {
   state = {
