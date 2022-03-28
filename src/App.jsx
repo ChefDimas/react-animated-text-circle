b5af9c04e51410ad168a777c3f8c87457d961f34
ChefDimas <dimalunik@aol.com>
2022-03-28 12:02:22 +0300

    Set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
-import NavBar from "./NavBar";
-import ShoppingCart from "./ShoppingCart";
 
 class App extends Component {
   render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <ShoppingCart />
-      </React.Fragment>
-    );
+    return <React.Fragment></React.Fragment>;
   }
 }
 
