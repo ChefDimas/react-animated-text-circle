da69cb1ee0578d0099aaa55c960f0d4387a79a69
ChefDimas <dimalunik@aol.com>
2022-03-29 21:18:16 +0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./app.css";
 function App() {
-  const [count, setCount] = useState(0);
+  //  Change const to modify the TEXT
+  const yourText = "Circle-text-animated";
 
   useEffect(() => {
-    document.title = `Вы нажали ${count} раз`;
+    let text = document.querySelector(".text");
+    // Separate characters by <span>
+    text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+    let element = document.querySelectorAll("span");
+    for (let i = 0; i < element.length; i++) {
+      // Change value of integer to modify the letter spacing. More characters in text, less value!
+      element[i].style.transform = "rotate(" + i * 18 + "deg)";
+    }
   });
   return (
     <div>
-      <p>Вы нажали {count} раз</p>
-      <button onClick={() => setCount(count + 1)}>Нажми на меня</button>
+      <section>
+        <h2 className="text">{yourText}</h2>
+      </section>
     </div>
   );
 }
