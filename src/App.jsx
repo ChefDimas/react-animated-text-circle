91b6e4c0db3e6f52cfee748cc7463213ac51cde4
ChefDimas <dimalunik@aol.com>
2022-03-29 15:47:27 +0300

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 import "./app.css";
-import axios from "axios";
-
-// function animation() {
-const text = "Text effect";
-let result = text.replace(/\S/g, "<span>$&</span>");
-console.log(result);
-//   const element = document.querySelectorAll("span");
-//   for (let i = 0; i < element.length; i++) {
-//     element[i].style.transform = "rotate(" + i + "deg)";
-//   }
-//   console.log(text);
-// }
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <section>
-          <h2 className="text">{text}</h2>
+          <h2 className="text">A</h2>
         </section>
       </div>
     );
